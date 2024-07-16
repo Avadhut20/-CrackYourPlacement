58e8b607803d8104b0eaf69b13e0af4ae8a4924e
Avadhut20 <your-email@example.com>
2024-07-16 19:54:10 +0530

    Day3 done

---
+++ Day3/LinkedListCycle.java
@@ -0,0 +1,27 @@
+package Day3;
+
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class LinkedListCycle {
+    public boolean hasCycle(ListNode head) {
+        ListNode f= head;
+        ListNode s= head;
+        while(f!=null && f.next!=null){
+            f=f.next.next;
+            s=s.next;
+            if(f==s){
+                return true;
+            }
+        }
+        return false;
+    }
+}
